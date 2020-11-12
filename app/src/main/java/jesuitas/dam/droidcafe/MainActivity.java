951c09143dcba47340a43d1b3c6cd0af17bb49aa
Alvaro Rodriguez <dam16.2020@gmail.com>
2020-11-12 16:29:17 +0100

    Menu y Popup

--- app/src/main/java/jesuitas/dam/droidcafe/MainActivity.java
+++ app/src/main/java/jesuitas/dam/droidcafe/MainActivity.java
@@ -9,6 +9,8 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.ImageButton;
+import android.widget.PopupMenu;
 import android.widget.Toast;
 
 import org.w3c.dom.Text;
@@ -17,8 +19,6 @@ public class MainActivity extends AppCompatActivity {
 
     private String toast;
 
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,6 +26,35 @@ public class MainActivity extends AppCompatActivity {
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        ImageButton imageButton = findViewById(R.id.imageButton);
+        imageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                PopupMenu popup = new PopupMenu(MainActivity.this, v);
+                popup.getMenuInflater().inflate(R.menu.menu_popup, popup.getMenu());
+                popup.show();
+
+                popup.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+                    @Override
+                    public boolean onMenuItemClick(MenuItem item) {
+                        switch (item.getItemId()){
+                            case R.id.popup_edit:
+                                displayToast("popup Edit");
+                                return true;
+                            case R.id.popup_share:
+                                displayToast("popup Share");
+                                return true;
+                            case R.id.popup_delete:
+                                displayToast("popup Delete");
+                                return true;
+                            default:
+                                return false;
+                        }
+                    }
+                });
+            }
+        });
     }
 
     @Override
